ab8259993ed76459519fd77b56b0de939d655551
Said-Aabilla <said160aabilla@gmail.com>
2023-04-22 20:10:46 +0000

    done with script that adds aliases to queries

--- STACK-queries/clean.py
+++ STACK-queries/clean.py
@@ -69,8 +69,6 @@ def assign_table_aliases(from_clause, table_aliases):
     print(from_clause)
     print("tables with no aliases: ", tables_with_no_aliases)
 
-    print("assigning aliases: ", table_aliases)
-
     # Rewrite the query with aliases
     for table in from_clause:
         if isinstance(table, str):
@@ -78,14 +76,13 @@ def assign_table_aliases(from_clause, table_aliases):
             table_index = from_clause.index(table)
             from_clause[table_index] = {'value': table, 'name': table_alias}
 
-    print("new from clause: ", from_clause)
-
     return from_clause, tables_with_no_aliases
 
 
 def replace_table_names_with_aliases_in_where_clause(where_clause, table_aliases, tables_with_no_aliases):
     if isinstance(where_clause, list):
-        return [replace_table_names_with_aliases_in_where_clause(sub_clause, table_aliases, tables_with_no_aliases) for sub_clause
+        return [replace_table_names_with_aliases_in_where_clause(sub_clause, table_aliases, tables_with_no_aliases) for
+                sub_clause
                 in
                 where_clause]
     elif isinstance(where_clause, dict):
@@ -101,51 +98,97 @@ def replace_table_names_with_aliases_in_where_clause(where_clause, table_aliases
 
                     left_table_name = value[0].split('.')[0]
                     if left_table_name in tables_with_no_aliases:
-                        print("clause before 00", where_clause[key])
+                        # print("clause before 00", where_clause[key])
                         left_new_value = f"{table_aliases[left_table_name]}.{value[0].split('.')[1]}"
                         new_clause[key] = [left_new_value, right_new_value]
-                        print("clause after 00", new_clause[key])
+                        # print("clause after 00", new_clause[key])
 
                     right_table_name = value[1].split('.')[0]
                     if right_table_name in tables_with_no_aliases:
-                        print("clause before 01 ", where_clause[key])
+                        # print("clause before 01 ", where_clause[key])
                         right_new_value = f"{table_aliases[right_table_name]}.{value[1].split('.')[1]}"
                         new_clause[key] = [left_new_value, right_new_value]
-                        print("clause after 01", new_clause[key])
+                        # print("clause after 01", new_clause[key])
 
                 elif isinstance(value[0], str) and '.' in value[0]:
                     table_name = value[0].split('.')[0]
                     if table_name in tables_with_no_aliases:
-                        print("clause before 11", where_clause[key])
+                        # print("clause before 11", where_clause[key])
                         new_value = [f"{table_aliases[table_name]}.{value[0].split('.')[1]}", value[1]]
                         new_clause[key] = new_value
-                        print("clause after 11", new_value)
+                        # print("clause after 11", new_value)
                     else:
                         new_clause[key] = value
 
                 elif isinstance(value[1], str) and '.' in value[1]:
                     table_name = value[1].split('.')[0]
                     if table_name in tables_with_no_aliases:
-                        print("clause before 22 ", where_clause[key])
+                        # print("clause before 22 ", where_clause[key])
                         new_value = [value[0], f"{table_aliases[table_name]}.{value[1].split('.')[1]}"]
                         new_clause[key] = new_value
-                        print("clause after 22", new_clause[key])
+                        # print("clause after 22", new_clause[key])
                     else:
                         new_clause[key] = value
                 else:
-                    print('else', value)
+                    # print('else', value)
                     new_clause[key] = value
             else:
-                new_clause[key] = replace_table_names_with_aliases_in_where_clause(value, table_aliases, tables_with_no_aliases)
+                new_clause[key] = replace_table_names_with_aliases_in_where_clause(value, table_aliases,
+                                                                                   tables_with_no_aliases)
         return new_clause
     else:
         return where_clause
 
 
+def replace_table_names_with_aliases_in_select_clause(select_clause, aliases, tables_with_no_aliases):
+    def replace_value_with_alias(value):
+        if isinstance(value, str):
+            left = value.split('.')[0]
+            right = value.split('.')[1]
+            print(left, right)
+            if (left in tables_with_no_aliases):
+                return f"{aliases[left]}.{right}"
+            return value
+
+        elif isinstance(value, dict):
+            for k, v in value.items():
+                value[k] = replace_value_with_alias(v)
+        return value
+
+    # when you have a single selection like: select count(distinct account.display_name)
+    # select_clause is like this: {'value': {'count': {'distinct': {'value': 'account.display_name'}}}}
+    if isinstance(select_clause, dict):
+        select_clause = {'value': replace_value_with_alias(select_clause['value'])}
+    # when you have multiple selection like: select count(distinct account.display_name), tag.name, comment.id
+    # select_clause is like this:  [{'value': {'count': {'distinct': {'value': 'account.display_name'}}}},
+    # {'value': 'tag.name'}, {'value': 'comment.id'}]
+    elif isinstance(select_clause, list):
+        for item in select_clause:
+            item['value'] = replace_value_with_alias(item['value'])
+    return select_clause
+
+
+def replace_table_names_with_aliases(select_clause, table_aliases):
+    def replace_value_with_alias(value):
+        if isinstance(value, dict):
+            if 'value' in value:
+                value['value'] = replace_value_with_alias(value['value'])
+            if 'name' in value and value['name'] not in table_aliases:
+                value['name'] = table_aliases.get(value['name'], value['name'])
+        elif isinstance(value, list):
+            value = [replace_value_with_alias(v) for v in value]
+        return value
+
+    if isinstance(select_clause, list):
+        for item in select_clause:
+            item['value'] = replace_value_with_alias(item['value'])
+    elif isinstance(select_clause, dict):
+        select_clause['value'] = replace_value_with_alias(select_clause['value'])
+
+    return select_clause
+
 
 if __name__ == '__main__':
-
-
     table_aliases = {'account': 'acc', 'so_user': 'u', 'tag': 't', 'comment': 'c', 'site': 's', 'tag_question': 'tq',
                      'answer': 'a', 'question': 'q', 'post_link': 'pl', 'badge': 'b'}
 
@@ -158,7 +201,18 @@ if __name__ == '__main__':
         select_clause = parsed_query['select']
         from_clause = parsed_query['from']
         where_clause = parsed_query['where']
+        print(select_clause)
 
-        new_from_clause, tables_with_no_aliases = assign_table_aliases(from_clause,table_aliases)
-        new_where_clause = replace_table_names_with_aliases_in_where_clause(where_clause, table_aliases, tables_with_no_aliases)
+        new_from_clause, tables_with_no_aliases = assign_table_aliases(from_clause, table_aliases)
+        new_where_clause = replace_table_names_with_aliases_in_where_clause(where_clause, table_aliases,
+                                                                            tables_with_no_aliases)
+        new_select_clause = replace_table_names_with_aliases_in_select_clause(select_clause, table_aliases,
+                                                                              tables_with_no_aliases)
 
+        parsed_query['select'] = new_select_clause
+        parsed_query['from'] = new_from_clause
+        parsed_query['where'] = new_where_clause
+
+        rewritten_query = moz_sql_parser.format(parsed_query)
+
+        create_file('plus', 'ffff.sql', rewritten_query)
