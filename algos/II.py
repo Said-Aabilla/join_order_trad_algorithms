82c40e4d573ee25e4d11f1793fa9ee9d7d2d37db
Said-Aabilla <said160aabilla@gmail.com>
2023-04-28 15:06:49 +0100

    modified ii

--- algos/II.py
+++ algos/II.py
@@ -1,36 +1,50 @@
+from algos.SA import get_join_conds
 from algos.helper_functions import get_join_order_cost, neighborhood
 import moz_sql_parser
 import itertools
 
 
-def iterative_improvement(query, num_permutations ):
+def iterative_improvement(query, num_permutations,filename ):
 
     # Parse the query and extract the table names
     parsed_query = moz_sql_parser.parse(query)
     tables = parsed_query['from']
 
     # Initialize the best join order and its cost
-    best_join_order = tables.copy()
-    best_cost = get_join_order_cost(parsed_query, best_join_order)
+    best_join_order = get_join_conds(parsed_query)
+    try:
+        best_cost = get_join_order_cost(query, best_join_order)
+    except:
+        best_cost = float('inf')
+
+    print(f"-------init------------------------------{best_cost}--")
 
     # Loop over all possible starting join orders
     index = 0
     for starting_join_order in itertools.islice(itertools.permutations(tables), num_permutations):
         index += 1
-        print("mother loop index", index)
+        # print("mother loop index", index)
         # Initialize the current join order and its cost
         current_join_order = list(starting_join_order)
-        current_cost = get_join_order_cost(parsed_query, current_join_order)
+        try:
+            current_cost = get_join_order_cost(query, current_join_order)
+        except:
+            current_cost = float('inf')
 
-
-        # Generate all possible adjacent join orders
+        # Generate possible adjacent join orders
         neighbors = neighborhood(current_join_order)
 
         improved = False
         # Evaluate the cost of each neighbor
         for neighbor in neighbors:
 
-                neighbor_cost = get_join_order_cost(parsed_query, neighbor)
+                try:
+                    neighbor_cost = get_join_order_cost(query, neighbor)
+                except:
+                    neighbor_cost =  float('inf')
+                    print(f"--------------------------------------------{filename}-----------------------------------------------")
+                    continue
+
 
                 # If the neighbor has a lower cost, select it as the new join order
                 if neighbor_cost < current_cost:
@@ -43,7 +57,7 @@ def iterative_improvement(query, num_permutations ):
                         improved = True
                         best_join_order = current_join_order.copy()
                         best_cost = current_cost
-                        print("current best_cost: ", best_cost)
+                        print("best_cost: ", best_cost)
                 if(improved):
                     break
         # if(improved):
